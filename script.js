cffd93a73b12f4ef71636aaba4cd81632f6a4ebb
Rithik Jakhmola <jakhmolarithik8@gmail.com>
2025-08-05 18:30:38 +0530

    this is the begining of the library project

---
+++ script.js
@@ -0,0 +1,7 @@
+const books = []; 
+function books (name , pages ,author ,  readStatus){
+    this.name = name ;
+    this.pages = pages ; 
+    this.author = author ; 
+    this.readStatus = readStatus ; 
+}
